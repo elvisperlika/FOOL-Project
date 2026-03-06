0646f2ed0eef128530d29d841b6a3c68ea01a7cb
Matteo Susca <matteo.susca@studio.unibo.it>
2026-03-06 14:45:22 +0100

    method node ASTGenerationSTVisitor

--- src/compiler/ASTGenerationSTVisitor.java
+++ src/compiler/ASTGenerationSTVisitor.java
@@ -289,6 +289,36 @@ public class ASTGenerationSTVisitor extends FOOLBaseVisitor<Node> {
   }
 
   @Override
+  public Node visitMethdec(MethdecContext c) {
+    if (print) printVarAndProdName(c);
+
+    // Visit all parameters
+    List<ParNode> parList = new ArrayList<>();
+    // Start from 1 because the first ID is the method's name
+    for (int i = 1; i < c.ID().size(); i++) {
+      ParNode p = new ParNode(c.ID(i).getText(), (TypeNode) visit(c.type(i)));
+      p.setLine(c.ID(i).getSymbol().getLine());
+      parList.add(p);
+    }
+    // Visit all local declarations
+    List<DecNode> decList = new ArrayList<>();
+    for (DecContext dec : c.dec()) decList.add((DecNode) visit(dec));
+
+    Node n = null;
+    if (!c.ID().isEmpty()) { //non-incomplete ST
+      n = new MethodNode(
+          c.ID(0).getText(),
+          (TypeNode) visit(c.type(0)),
+          parList,
+          decList,
+          visit(c.exp())
+      );
+      n.setLine(c.FUN().getSymbol().getLine());
+    }
+    return n;
+  }
+
+  @Override
   public Node visitNew(NewContext c) {
     if (print) printVarAndProdName(c);
     String id = c.ID().getText();
